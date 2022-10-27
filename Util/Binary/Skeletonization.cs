c7498e13db58b4f50a3ca4b800163d9fa70cecb8
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-10-27 14:49:23 +0700

    i got all the references i need

---
+++ Util/Binary/Skeletonization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmguCvUtils.Util.Binary
+{
+    /*
+    reference: 
+        https://homepages.inf.ed.ac.uk/rbf/HIPR2/skeleton.htm
+        http://cgm.cs.mcgill.ca/~godfried/teaching/projects97/azar/skeleton.html
+        https://www.iasj.net/iasj/download/bff0412e0fae0dee
+    */
+    internal class Skeletonization
+    {
+    }
+}
